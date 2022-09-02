6e70d17f8830f2122f35a00a3d4bc8968737a8ae
Henrique Melo <Henrique-125@hotmail.com>
2022-09-02 13:56:43 -0300

    Navbar / Login / Footer

--- blog-pessoal/src/App.tsx
+++ blog-pessoal/src/App.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
 import './App.css';
 
-function App() {
-  return(
 
-    <Home />
-    
-    )
-    
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <div style={{ minHeight: '100vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+
+    </>
+  )
+
 }
 
 export default App;
---
+++ blog-pessoal/src/components/static/navbar/Navbar.tsx
@@ -0,0 +1,51 @@
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Box className='cursor'>
+                        <Typography variant="h5" color="inherit">
+                            Blogpessoal
+                        </Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="start">
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                <Link to="/home">Home</Link>
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                postagens
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                temas
+                            </Typography>
+                        </Box>
+                        <Box mx={1} className='cursor'>
+                            <Typography variant="h6" color="inherit">
+                                cadastrar
+                            </Typography>
+                        </Box>
+                        <Link to='/login' className='text-decorator-none'>
+                            <Box mx={1} className='cursor'>
+                                <Typography variant="h6" color="inherit">
+                                    logout
+                                </Typography>
+                            </Box>
+                        </Link>
+                    </Box>
+                </Toolbar>
+            </AppBar>
+        </>
+    );
+}
+
+export default Navbar;
